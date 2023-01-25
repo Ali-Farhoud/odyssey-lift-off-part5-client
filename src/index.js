91955a622b0295c8f874ebf5a6737bc60eaae4f7
Ali Farhoud <alifarhoud613@gmail.com>
2023-01-25 16:39:23 +0300

    testw

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import GlobalStyles from './styles'
 import Pages from './pages'
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client' //c
 
 const client = new ApolloClient({
 	uri: 'https://odyssey-lift-off-part5-server-production-5a59.up.railway.app/', // change to YOUR own production server
