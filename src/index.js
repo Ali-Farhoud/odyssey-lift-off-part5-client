cac18c71b3fde3ea58e1c1600527f33e3fb23c15
Ali Farhoud <alifarhoud613@gmail.com>
2023-01-25 16:25:08 +0300

    change uri

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import GlobalStyles from "./styles";
-import Pages from "./pages";
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import GlobalStyles from './styles'
+import Pages from './pages'
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
-  uri: "https://space-courses-api.herokuapp.com/", // change to YOUR own production server
-  cache: new InMemoryCache(),
-  name: "web",
-  version: "1.0",
-});
+	uri: 'https://odyssey-lift-off-part5-server-production-5a59.up.railway.app/', // change to YOUR own production server
+	cache: new InMemoryCache(),
+	name: 'web',
+	version: '1.0',
+})
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <GlobalStyles />
-    <Pages />
-  </ApolloProvider>,
-  document.getElementById("root")
-);
+	<ApolloProvider client={client}>
+		<GlobalStyles />
+		<Pages />
+	</ApolloProvider>,
+	document.getElementById('root')
+)
